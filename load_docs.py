615e6143746461fdd3a403466e4bd52181a4e389
Chay0803 <148848447+Chay0803@users.noreply.github.com>
2025-06-17 14:56:00 +0530

    Update load_docs.py

--- load_docs.py
+++ load_docs.py
@@ -2,7 +2,7 @@ import os
 from langchain_community.document_loaders import PyPDFLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import FAISS
-from langchain_community.embeddings import HuggingFaceBgeEmbeddings
+from langchain_community.embeddings import HuggingFaceEmbeddings
 
 def get_vectorstore(data_path="data"):
     documents = []
